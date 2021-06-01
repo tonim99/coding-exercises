1308da8e60c6e5719f81dc7aa3fce1f00437cba6
Tonimarie Morris <morris.tonimarie@gmail.com>
2021-06-01 03:40:20 -0400

    first commit

---
+++ protein-translation/protein-translation.js
@@ -0,0 +1,7 @@
+//
+// This is only a SKELETON file for the 'Protein Translation' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+export const translate = () => {
+  
+}
